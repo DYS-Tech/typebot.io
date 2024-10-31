a2068f3047c03db46bbf446a4a7ee7e047c91289
freonzx <freonzx@gmail.com>
2024-10-31 19:27:28 -0300

    add emails allowed

--- packages/lib/src/google.ts
+++ packages/lib/src/google.ts
@@ -1,7 +1,7 @@
 import { env } from "@typebot.io/env";
 import prisma from "@typebot.io/prisma";
 import type { Prisma } from "@typebot.io/prisma/types";
-import { type Credentials, OAuth2Client } from "google-auth-library";
+import { type Credentials, OAuth2Client, TokenPayload } from "google-auth-library";
 import { decrypt } from "./api/encryption/decrypt";
 import { encrypt } from "./api/encryption/encrypt";
 import { isDefined } from "./utils";
@@ -21,6 +21,26 @@ export const getAuthenticatedGoogleClient = async (
     `${env.NEXTAUTH_URL}/api/credentials/google-sheets/callback`,
   );
   oauth2Client.setCredentials(data);
+
+    // Verificação do email permitido
+    if ('id_token' in data) {
+      const ticket = await oauth2Client.verifyIdToken({
+        idToken: (data as any).id_token,
+        audience: env.GOOGLE_CLIENT_ID,
+      })
+  
+      const payload = ticket.getPayload() as TokenPayload
+  
+      // Allow multiple emails
+      const allowedEmails: string[] = ['dys.tech.br@gmail.com', 'admvuala@gmail.com']
+  
+      if (allowedEmails.indexOf(payload.email ?? '') === -1) {
+        throw new Error('Unauthorized')
+      }
+    } else {
+      throw new Error('ID token is missing in the credentials data')
+    }
+
   oauth2Client.on("tokens", updateTokens(credentialsId, data));
   return oauth2Client;
 };
